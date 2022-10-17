3f36583cf2b21fec621c77143d8074a929135589
sheriffahmy96 <100234310+sheriffahmy96@users.noreply.github.com>
2022-10-16 19:09:48 -0500

    Create main.py
    Day 2 Simple BMI Calculator

---
+++ Day2_BMICalculator/main.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+
+
+#Write your code below this line 👇
+
+
+BMI_height = float(height) ** 2
+print(int(int(weight) / BMI_height))
